f4a4e94101cea4d7e52d749c7f0025adb2490f92
Sazzad Nibir <sazzadnibir@hotmail.com>
2023-04-24 18:34:15 +0600

    Experimentaing with sessions

--- go.mod
+++ go.mod
@@ -2,9 +2,8 @@ module github.com/sazzadnibir/go_modules
 
 go 1.20
 
-require github.com/go-chi/chi/v5 v5.0.8
-
 require (
-	github.com/alexedwards/scs/v2 v2.5.1 // indirect
-	github.com/justinas/nosurf v1.1.1 // indirect
+	github.com/alexedwards/scs/v2 v2.5.1
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/justinas/nosurf v1.1.1
 )
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -25,12 +25,18 @@ func NewHandalers(r *Repository) {
 }
 
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIp := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIp)
+
 	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
 }
 
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello, again."
+
+	remoteIp := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIp
 
 	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
 		StringMap: stringMap,
